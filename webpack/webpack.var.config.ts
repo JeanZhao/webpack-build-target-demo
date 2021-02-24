5061bafd351bcca2d8d0acc93bd5834632e0c6ae
JeanZhao <jeanzhao@aliyun.com>
2021-02-24 17:37:46 +0800

    add webpack config

---
+++ webpack/webpack.var.config.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import webpack from "webpack";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: 'var.js',
+    library: "myDemo",
+    libraryTarget: 'var'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+   // new CleanWebpackPlugin(),
+  ],
+};
+
+export default config;
